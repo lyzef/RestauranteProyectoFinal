e9cc364663e5d0b1b577053fd71757f033c34a0f
Miguel <bautistaortegam4@gmail.com>
2026-02-18 14:20:40 -0700

    Cambio en interfaz de inicio de sesion

--- src/login/Ventana.java
+++ src/login/Ventana.java
@@ -1,17 +1,12 @@
 package login;
 
-import java.awt.Color;
-import java.awt.Font;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
 	public Ventana() {
-		setSize(500,500); //Establece el tamaño
+		setSize(500,400); //Establece el tamaño
 		//Termina la ejecución del programa al cerrar la ventana.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//Establece el lugar donde aparecerá la ventana
@@ -26,40 +21,59 @@ public class Ventana extends JFrame{
 		setLocationRelativeTo(null); //A mitad de pantalla
 		setVisible(false); //Establece visibilidad
 		
-		setLayout(null);
-		JButton boton = new JButton("Ingresar");
-		boton.setBounds(10,290,200,50);
-		add(boton);
+		//this.setLayout(new BorderLayout()); Layout por defecto de JFrame
 		
-		JLabel label = new JLabel("Ingrese su correo electronico");
-		label.setFont(new Font("arial",Font.PLAIN,20));
-		label.setBounds(10,20,500,100);
-		add(label);
+		//Organizacion de paneles
+		JPanel panelNorteContenedor = new JPanel();
+		panelNorteContenedor.setLayout(new BoxLayout(panelNorteContenedor, BoxLayout.Y_AXIS));
+		JPanel panelCentroEntrada = new JPanel(new GridLayout(6, 1, 10, 10));
+		JPanel panelSurEntrada = new JPanel();
 		
-		JTextField textField = new JTextField();
-		textField.setBounds(10,100,200,50);
-		textField.setFont(new Font("Arial",Font.PLAIN,20));
-		add(textField);
+		//Panel norte
+		JLabel labelNombreRestaurante = new JLabel("Madero's");
+		labelNombreRestaurante.setFont(new Font("Times", Font.PLAIN, 24));
+		labelNombreRestaurante.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    panelNorteContenedor.add(labelNombreRestaurante);
 		
-		JLabel label1 = new JLabel("Ingrese su contraseña");
-		label1.setFont(new Font("arial",Font.PLAIN,20));
-		label1.setBounds(10,160,500,100);
-		add(label1);
+		JLabel labelTitulo = new JLabel("Inicio de sesion");
+	    labelTitulo.setFont(new Font("Arial", Font.PLAIN, 16));
+	    labelTitulo.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    panelNorteContenedor.add(labelTitulo);
 		
-		JPasswordField Password = new JPasswordField();
-		Password.setBounds(10,230,200,50);
-		Password.setFont(new Font("Arial",Font.PLAIN,20));
-		add(Password);
-		
-		
-		//Mal contraseña
-		JLabel mensajeError = new JLabel("Usuario inexistente o contraseña incorrecta");
-		mensajeError.setFont(new Font("arial",Font.PLAIN,15));
-		mensajeError.setBounds(10,300,500,100);
-		mensajeError.setForeground(Color.RED);
-		add(mensajeError);
-		
-		setVisible(true);
+	    panelNorteContenedor.setBorder(BorderFactory.createEmptyBorder(10,5,5,5));
+	    
+	    //Panel centro
+        JLabel labelCorreo = new JLabel("Ingrese el correo electrónico");
+        labelCorreo.setFont(new Font("Arial", Font.PLAIN, 16));
+        panelCentroEntrada.add(labelCorreo);
+
+        JTextField textField = new JTextField(20);
+        textField.setFont(new Font("Arial", Font.PLAIN, 14));
+        panelCentroEntrada.add(textField);
+
+        JLabel labelPass = new JLabel("Ingrese la contraseña");
+        labelPass.setFont(new Font("Arial", Font.PLAIN, 16));
+        panelCentroEntrada.add(labelPass);
+
+        JPasswordField password = new JPasswordField(20);
+        password.setFont(new Font("Arial", Font.PLAIN, 14));
+        panelCentroEntrada.add(password);
+        panelCentroEntrada.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        
+        //Panel sur
+        JButton boton = new JButton("Entrar");
+        boton.setFont(new Font("Arial", Font.PLAIN, 14));
+        panelSurEntrada.add(boton);
+        
+       
+        
+        //Layouts
+        add(panelNorteContenedor, BorderLayout.NORTH);
+        add(panelCentroEntrada, BorderLayout.CENTER);
+        add(panelSurEntrada, BorderLayout.SOUTH);
+        
+        setVisible(true);
+        
 		
 	}
 }
